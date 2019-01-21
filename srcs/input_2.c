215d24bcc15b0d1f9fcc5d8c04894da43c1f0578
Lu-nung  Lee <llee@e1z2r2p2.42.us.org>
2019-01-21 15:55:34 -0800

    finalized input

--- srcs/input_2.c
+++ srcs/input_2.c
@@ -45,9 +45,9 @@ void	color(t_mlx *mlx, int key)
 	if (key == 21)
 		mlx->map->c_min = 0x00FF00;
 	if (key == 22)
-		mlx->map->c_min = 0x0000FF;
-	if (key == 23)
 		mlx->map->c_min = 0x4B0082;
+	if (key == 23)
+		mlx->map->c_min = 0x0000FF;
 	if (key == 26)
 		mlx->map->c_min = 0x9400D3;
 	render(mlx);
